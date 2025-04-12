6f76f89ecb71df130537c82e8ff798679ae223ba
recharge-website-team <cvignesh404@gmail.com>
2025-04-12 14:09:59 +0530

    Added backend api

--- frontend/src/hooks/useQueryData.ts
+++ frontend/src/hooks/useQueryData.ts
@@ -1,7 +1,7 @@
 import { QueryKey, useQuery, UseQueryOptions } from '@tanstack/react-query'
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8000'
+const API_URL = import.meta.env.VITE_BACKEND_ENDPOINT;
 
 const api = axios.create({
     baseURL: API_URL,
